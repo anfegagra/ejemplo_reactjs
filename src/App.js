78074265b7026c0d720f3f12cf31ec45157249b5
Andrés Felipe García Granados <garciapipe1994@gmail.com>
2020-05-24 17:28:37 -0500

    otra forma de crear un componente por medio de la función flecha

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Hello(props) {
-  return <h2>{props.oe}</h2>;
-}
+// function Hello(props) {
+//   return <h2>{props.oe}</h2>;
+// }
+
+const Hello = (props) => <h2>{props.title}</h2>;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Hello oe='Hola mundo en ReactJS' />
+        <Hello title='Hola mundo en ReactJS' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
